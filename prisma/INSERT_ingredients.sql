9f88459d886451efe5b7ffb8b63b151011f6b04f
renatosans <renato@datacopy.com.br>
2023-02-08 18:34:05 -0300

    Inclusão de assets

---
+++ prisma/INSERT_ingredients.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO ingredients(ingredient_name,flag,img) VALUES ('Calabresa',true,'https://img.freepik.com/free-photo/fresh-raw-sausage-old-wooden_2829-15934.jpg');
+INSERT INTO ingredients(ingredient_name,flag,img) VALUES ('Ovo',true,'https://img.freepik.com/free-photo/top-view-duck-eggs-dark-surface_1150-36985.jpg');
+INSERT INTO ingredients(ingredient_name,flag,img) VALUES ('Tomate',true,'https://img.freepik.com/free-photo/fresh-tomatoes-ready-cook_1150-38243.jpg');
+INSERT INTO ingredients(ingredient_name,flag,img) VALUES ('Mussarela',true,'https://img.freepik.com/free-photo/isometric-cheese-composition_23-2148161904.jpg');
+INSERT INTO ingredients(ingredient_name,flag,img) VALUES ('Azeitona',true,'https://img.freepik.com/free-photo/fresh-tasty-green-olives_1220-1414.jpg');
+INSERT INTO ingredients(ingredient_name,flag,img) VALUES ('Peperoni',true,'https://img.freepik.com/free-photo/tasty-traditional-chorizo-assortment_23-2148980296.jpg');
+INSERT INTO ingredients(ingredient_name,flag,img) VALUES ('Presunto',true,'https://img.freepik.com/free-photo/pork-ham_1339-2076.jpg');
