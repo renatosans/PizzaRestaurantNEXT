eb36bc9f195c4b268cc961881595167fce6f2070
renatosans <renato@datacopy.com.br>
2023-02-08 20:38:15 -0300

    Alteração para Postgres

--- utils/connection.ts
+++ utils/connection.ts
@@ -2,16 +2,16 @@ import { PrismaClient } from '@prisma/client';
 
 
 const host     = 'localhost'
-const port     = 3306
-const username = 'root'
+const port     = 5432
+const username = 'postgres'
 const password = 'p@ssw0rd'
 const database = 'pizza_ordering'
 const ssl      = false
 const setSSL   = 'sslaccept=strict&sslmode=require'
 
 
-// DATABASE_URL="mysql://root:p@ssw0rd@localhost:3306/pizza_ordering"
-let url = `mysql://${username}:${password}@${host}:${port}/${database}`;
+// DATABASE_URL="postgresql://postgres:p@ssw0rd@localhost:5432/pizza_ordering"
+let url = `postgresql://${username}:${password}@${host}:${port}/${database}`;
 if (ssl) {
     url = url + `?${setSSL}`;
 }
