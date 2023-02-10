72d308b18a2b1c2f099a0e774f55a876adfd5af4
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-02-10 18:33:49 -0300

    Inclusão de massa de dados

---
+++ prisma/INSERT_pizzas.sql
@@ -0,0 +1,12 @@
+/* -- Date: 2023-02-10 18:18 */
+
+INSERT INTO pizza (name,description,imageSrc,heat,price,discount,currency) VALUES ('Sicilian Pizza','Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in...','/pizza1.jpg',3,15.00,NULL,'$');
+INSERT INTO pizza (name,description,imageSrc,heat,price,discount,currency) VALUES ('Havaian','Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in...','/pizza2.jpg',2,9.00,NULL,'$');
+INSERT INTO pizza (name,description,imageSrc,heat,price,discount,currency) VALUES ('Detroit Pizza','Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in...','/palermo2.jpg',1,25.00,-10.00,'$');
+INSERT INTO pizza (name,description,imageSrc,heat,price,discount,currency) VALUES ('Chicago Pizza','Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in...','/pizza3.jpg',3,15.00,NULL,'$');
+INSERT INTO pizza (name,description,imageSrc,heat,price,discount,currency) VALUES ('Greek Pizza','Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in...','/pizza4.jpg',2,9.00,NULL,'$');
+INSERT INTO pizza (name,description,imageSrc,heat,price,discount,currency) VALUES ('Types of Pizza Crust','Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in...','/pizza5.jpg',2,9.00,NULL,'$');
+INSERT INTO pizza (name,description,imageSrc,heat,price,discount,currency) VALUES ('Neapolitan Pizza','Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in...','/palermo.jpg',3,15.00,NULL,'$');
+INSERT INTO pizza (name,description,imageSrc,heat,price,discount,currency) VALUES ('California Pizza','Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in...','/pizza6.jpg',2,9.00,NULL,'$');
+INSERT INTO pizza (name,description,imageSrc,heat,price,discount,currency) VALUES ('New York-Style Pizza','Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in...','/pizza7.jpg',2,9.00,NULL,'$');
+INSERT INTO pizza (name,description,imageSrc,heat,price,discount,currency) VALUES ('Volcano','Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in...','/pizza8.jpg',2,9.00,NULL,'$');
