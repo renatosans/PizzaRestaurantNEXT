0bf001f7cff8cb029bc61e31cc66cdd5193f3d33
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-02-10 18:13:07 -0300

    Criação de endpoint na API

---
+++ pages/api/pizzas/index.ts
@@ -0,0 +1,26 @@
+import { prisma } from '../../../utils/connection'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	switch (req.method) {
+		case "POST": {
+			return savePizza(req, res);
+		}
+		case "GET": {
+			return getPizzas(req, res);
+		}
+	}
+}
+
+const savePizza = async (req: NextApiRequest, res: NextApiResponse) => {
+    prisma.pizza.create({ data: req.body })
+    .then((result) => res.send(result))
+	.catch((error) => res.send("Error: " + error.message))
+}
+
+const getPizzas = async (req: NextApiRequest, res: NextApiResponse) => {
+    prisma.pizza.findMany()
+    .then((pizzas) => res.send(pizzas))
+    .catch((error) => res.send("Error: " + error.message))
+}
