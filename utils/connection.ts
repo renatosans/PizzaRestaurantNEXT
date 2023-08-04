5b4aa8b0c061fb6ebad091d86598693b642de90a
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-08-04 09:57:35 -0300

    Alterado para MySQL

--- utils/connection.ts
+++ utils/connection.ts
@@ -1,17 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
 
-const host       = 'localhost'
-const port       = 27017
-const database   = 'pizza_ordering'
-const direct     = true
-const setDirect  = 'directConnection=true'
+const host     = 'localhost'
+const port     = 3306
+const username = 'root'
+const password = 'p@ssw0rd'
+const database = 'pizza_ordering'
+const ssl      = false
+const setSSL   = 'sslaccept=strict&sslmode=require'
 
 
-// DATABASE_URL="mongodb://localhost:27017/pizza_ordering?directConnection=true"
-let url = `mongodb://${host}:${port}/${database}`;
-if (direct) {
-    url = url + `?${setDirect}`;
+// DATABASE_URL="mysql://root:p@ssw0rd@localhost:3306/pizza_ordering"
+let url = `mysql://${username}:${password}@${host}:${port}/${database}`;
+if (ssl) {
+    url = url + `?${setSSL}`;
 }
 
 const prisma = new PrismaClient({datasources: { db: { url: url } } })
